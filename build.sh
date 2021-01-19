f0ad8e7242e4aecbd8e0bd280fb3bc808ae690e2
Alex <43142871+khyberspache@users.noreply.github.com>
2021-01-19 15:07:01 -0500

    Build all script (#1)
    * changing os to or for conditional statement
    * adds error handling on closed/unavailable sockets
    * build script for build server

--- build.sh
+++ build.sh
@@ -5,31 +5,26 @@
 ## This script compiles the Nicodemus implant and plugins for the host OS + Windows.
 ## Ensure you have MinGW installed.
 ## To cross-compile to a MacOS build target please see the project README.
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux amd64
-    nim c --cpu:amd64 -d:release -d:ssl --opt:size -o:nicodemus-linux-amd64 --outdir:./payloads main.nim
-    # Compile Linux plugins.
-    for plugin in $(find module/plugin/ -name "*.nim"); do
-        plugin_name=$(basename -s .nim $plugin)
-        plugin_dir=$(dirname $plugin| cut -d '/' -f2-)
-        nim c --cpu:amd64 -d:release -d:ssl --opt:size -o:$plugin_name-linux-amd64 --outdir:./payloads/$plugin_dir $plugin;
-    done
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # MacOS amd64
-    nim c --cpu:amd64 -d:release -d:ssl --opt:size -o:nicodemus-darwin-amd64 --outdir:./payloads main.nim
-    # Compile MacOS plugins.
-    for plugin in $(find module/plugin/ -name "*.nim"); do
-        plugin_name=$(basename -s .nim $plugin)
-        plugin_dir=$(dirname $plugin| cut -d '/' -f2-)
-        nim c --cpu:amd64 -d:release -d:ssl --opt:size -o:$plugin_name-darwin-amd64 --outdir:./payloads/$plugin_dir $plugin;
-    done
-fi
 
-# Windows amd64
-nim c --cpu:amd64 -d:release -d:ssl --opt:size -d:mingw -o:nicodemus-windows-amd64 --outdir:./payloads main.nim
-# Compile Windows plugins.
+nimble install -y
+
+nim c --cpu:amd64 -d:release -d:ssl --os:linux --opt:size -o:nicodemus-linux --outdir:./payloads main.nim
 for plugin in $(find module/plugin/ -name "*.nim"); do
     plugin_name=$(basename -s .nim $plugin)
     plugin_dir=$(dirname $plugin| cut -d '/' -f2-)
-    nim c --cpu:amd64 -d:release -d:ssl --opt:size -d:mingw -o:$plugin_name-windows-amd64 --outdir:./payloads/$plugin_dir $plugin;
+    nim c --cpu:amd64 -d:release -d:ssl --os:linux --opt:size -o:$plugin_name-linux --outdir:./payloads/$plugin_dir $plugin;
+done
+
+nim c --cpu:amd64 -d:release -d:ssl --os:macosx --opt:size -o:nicodemus-darwin --outdir:./payloads main.nim
+for plugin in $(find module/plugin/ -name "*.nim"); do
+    plugin_name=$(basename -s .nim $plugin)
+    plugin_dir=$(dirname $plugin| cut -d '/' -f2-)
+    nim c --cpu:amd64 -d:release -d:ssl --os:macosx --opt:size -o:$plugin_name-darwin --outdir:./payloads/$plugin_dir $plugin;
+done
+
+nim c --cpu:amd64 -d:release -d:ssl --os:windows  --opt:size -d:mingw -o:nicodemus-windows.exe --outdir:./payloads main.nim
+for plugin in $(find module/plugin/ -name "*.nim"); do
+    plugin_name=$(basename -s .nim $plugin)
+    plugin_dir=$(dirname $plugin| cut -d '/' -f2-)
+    nim c --cpu:amd64 -d:release -d:ssl --os:windows --opt:size -d:mingw -o:$plugin_name-windows.exe --outdir:./payloads/$plugin_dir $plugin;
 done
